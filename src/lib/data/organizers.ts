730220e4bd316033daeb795a60ce228b8642da4c
Bruno Chaves <bruno@chaves.fr>
2026-02-25 16:20:24 +0100

    new photos 2

--- src/lib/data/organizers.ts
+++ src/lib/data/organizers.ts
@@ -1,16 +1,48 @@
 export interface Coordinator {
-	name: string;
-	years: string;
-	photo: string;
+  name: string;
+  years: string;
+  photo: string;
 }
 
 export const coordinators: Coordinator[] = [
-	{ name: 'Muriel Faverie', years: '2002–2006', photo: '/images/organizers/muriel.jpg' },
-	{ name: 'Maria Breidy', years: '2008–2013', photo: '/images/organizers/maria.jpg' },
-	{ name: 'Chiara Caccinelli', years: '2015–2019', photo: '/images/organizers/chiara.jpg' },
-	{ name: 'Marie-Hélène Caitucoli', years: '2022', photo: '/images/organizers/Marie-Helene.jpg' },
-	{ name: 'Hortense De Chabot-Tramecourt', years: '2023', photo: '/images/organizers/Hortense.jpg' },
-	{ name: 'Olga Hein', years: '2024-2025', photo: '/images/organizers/Olga.jpg' },
-	{ name: 'Justine Mougenot', years: '2026', photo: '/images/organizers/Justine.jpg' },
-	{ name: 'Bruno Chaves', years: 'from 2002', photo: '/images/organizers/bruno.jpg' }
+  {
+    name: "Muriel Faverie",
+    years: "2002–2006",
+    photo: "/images/organizers/muriel.jpg",
+  },
+  {
+    name: "Maria Breidy",
+    years: "2008–2013",
+    photo: "/images/organizers/maria.jpg",
+  },
+  {
+    name: "Chiara Caccinelli",
+    years: "2015–2019",
+    photo: "/images/organizers/chiara.jpg",
+  },
+  {
+    name: "Marie-Hélène Caitucoli",
+    years: "2022",
+    photo: "/images/organizers/Marie-Helene.jpg",
+  },
+  {
+    name: "Hortense De Chabot-Tramecourt",
+    years: "2023",
+    photo: "/images/organizers/Hortense.jpg",
+  },
+  {
+    name: "Olga Hein",
+    years: "2024-2025",
+    photo: "/images/organizers/Olga.jpg",
+  },
+  {
+    name: "Justine Mougenot",
+    years: "from 2026",
+    photo: "/images/organizers/Justine.jpg",
+  },
+  {
+    name: "Bruno Chaves",
+    years: "from 2002",
+    photo: "/images/organizers/bruno.jpg",
+  },
 ];
