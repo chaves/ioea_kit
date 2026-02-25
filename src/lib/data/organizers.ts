d49191c588c9b7ffd4d9f2cc270356912a183682
Bruno Chaves <bruno@chaves.fr>
2026-02-25 16:11:58 +0100

    new photos

--- src/lib/data/organizers.ts
+++ src/lib/data/organizers.ts
@@ -8,7 +8,9 @@ export const coordinators: Coordinator[] = [
 	{ name: 'Muriel Faverie', years: '2002–2006', photo: '/images/organizers/muriel.jpg' },
 	{ name: 'Maria Breidy', years: '2008–2013', photo: '/images/organizers/maria.jpg' },
 	{ name: 'Chiara Caccinelli', years: '2015–2019', photo: '/images/organizers/chiara.jpg' },
-	{ name: 'Marie-Hélène Caitucoli', years: 'from 2022', photo: '/images/organizers/Marie-Helene.jpg' },
+	{ name: 'Marie-Hélène Caitucoli', years: '2022', photo: '/images/organizers/Marie-Helene.jpg' },
+	{ name: 'Hortense De Chabot-Tramecourt', years: '2023', photo: '/images/organizers/Hortense.jpg' },
+	{ name: 'Olga Hein', years: '2024-2025', photo: '/images/organizers/Olga.jpg' },
+	{ name: 'Justine Mougenot', years: '2026', photo: '/images/organizers/Justine.jpg' },
 	{ name: 'Bruno Chaves', years: 'from 2002', photo: '/images/organizers/bruno.jpg' }
 ];
-
